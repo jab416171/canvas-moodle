719cd5c72574c4fbe470ea0f68fce8cc2976ecdb
Joseph Bass <jab416171@gmail.com>
2012-06-11 08:23:50 -0600

    Intitial commit with extension files

---
+++ js/contentscript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+    function init() {
+		var grade1 = "Class 1: 100%";
+		$("#right-side").append('<div class="events_list"><h2>Grade Summary</h2><ul class="right-side-list events"><li class="event">' + grade1 + '</l1></ul></div>');
+		
+    }
+	
+
+    init();
+});
