432d1fd204bede7b64852507df28db04125c4566
Joseph Bass <jab416171@gmail.com>
2012-06-11 08:30:31 -0600

    Grade summary showing up

--- js/contentscript.js
+++ js/contentscript.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 	
     function init() {
 		var grade1 = "Class 1: 100%";
-		$("#right-side").append('<div class="events_list"><h2>Grade Summary</h2><ul class="right-side-list events"><li class="event">' + grade1 + '</l1></ul></div>');
 		
+		$("#right-side").prepend('<div class="events_list"><h2>Grade Summary</h2><div id="grade-summary-div"></div></div>');
+		$("#grade-summary-div").load("grades .course_details");
     }
 	
 
