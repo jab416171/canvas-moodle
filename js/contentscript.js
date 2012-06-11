d74e5d1ea1f716df41c3c26b64e91c22916412e6
Joseph Bass <jab416171@gmail.com>
2012-06-11 08:42:11 -0600

    removed an unnecessary line and whitespace

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
 	
     function init() {
-		var grade1 = "Class 1: 100%";
-		
 		$("#right-side").prepend('<div class="events_list"><h2>Grade Summary</h2><div id="grade-summary-div"></div></div>');
 		$("#grade-summary-div").load("grades .course_details");
     }
-	
-
     init();
 });
