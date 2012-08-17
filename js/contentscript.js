37137d40a746d7e2dc9b4d8afcf8cf49d15ee25f
Colten Rouska <crouska@gmail.com>
2012-08-16 21:28:46 -0600

    Updated manifest, and contentscript

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,8 +1,43 @@
 $(document).ready(function() {
+
+	function login(){
+		
+	}
+	function removeElements(){
+		$("aside#right-side").children('div').remove();
+		$("aside#right-side").children('h2').remove();
+		$("aside#right-side").children('ul').remove();
+	}
 	
-    function init() {
-		$("#right-side").prepend('<div class="events_list"><h2>Grade Summary</h2><div id="grade-summary-div"></div></div>');
-		$("#grade-summary-div").load("grades .course_details");
+	function addDivs(){
+		$("aside#right-side").prepend('<div class="events_list"><h2>Upcoming Assignments</h2><div class="assignment-summary-div"></div></div>');
+		$("aside#right-side").prepend('<div class="events_list"><h2>Grade Summary</h2><div class="grade-summary-div"></div></div>');
+	}
+	
+    function auth() {
+		// $.ajax({
+			// type: 'GET',
+			// url: 'https://lms.neumont.edu/api/v1/courses',
+			// data:{
+				// 'client_id' : '5',
+				// 'response_type': 'code',
+				// 'redirect_uri' : 'urn:ietf:wg:oauth:2.0:oob'
+				//'Authorization': 'pVQ51V8VPAnWOrmo8i0Fy1sssV4MIzSy94O103LOZAFuqqe4V40LoM3QowuM4fND'
+			// },
+			// success: function(){
+				// alert('works');
+			// }
+		// });
+		
     }
-    init();
+	
+	function getInfo(){
+		$(".grade-summary-div").load("grades .course_details");
+		$(".assignment-summary-div").load("assignments div.assignment_list");
+	}
+	
+	removeElements();
+    addDivs();
+	//auth();
+	getInfo();
 });
