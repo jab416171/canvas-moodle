5d2a3baeab2df075012ea12d62348315116851d0
Colten Rouska <crouska@gmail.com>
2012-08-21 15:47:42 -0600

    Assignments and calendar changed slightly
    Removed images next to top of calendar - at least until I can figure out
    how to make it interactive.
    Changed what is getting called for assignements.

--- js/contentscript.js
+++ js/contentscript.js
@@ -6,26 +6,23 @@ $(document).ready(function() {
 	}
 	
 	function addDivs(){
-		$("aside#right-side").prepend('<div class="events_list"><h2>Upcoming Assignments</h2><div class="assignment-summary-div"></div></div>');
+		$("aside#right-side").prepend('<div class="assignments"><h2><a style="float: right; font-size: 10px; font-weight: normal;" class="event-list-view-calendar small-calendar" href="https://lms.neumont.edu/calendar">View Calendar</a>Upcoming Assignments</h2><div class="assignment-summary-div"></div></div>');
 		$("aside#right-side").prepend('<div class="events_list"><h2>Grade Summary</h2><div class="grade-summary-div"></div></div>');
 		$("aside#right-side").prepend('<div class="calendar"><h2>Calendar</h2><div class="calendar-div"></div></div>');
 	}
-	
+
     function getAssignments() {
 		$.ajax({
 			type: 'GET',
 			url: 'https://lms.neumont.edu/assignments',
 			success: function(data){
 				var response = $(data);
-				var upcoming = response.find(".assignment_list:eq(2)");
-				var firstFive = upcoming.children('div').slice(0,5);
-				firstFive.children('h2').remove();
-				firstFive.find("div.move").remove();
-				firstFive.find("div.links").remove();
-				firstFive.find("div.details").remove();
-				firstFive.find("div.content").append('<hr>');
-				
-				$(".assignment-summary-div").html(firstFive);
+				var upcoming = response.find("div.events_list");
+
+				upcoming.children("h2").remove();
+				upcoming.find("div.show-only-from").remove();
+
+				$(".assignment-summary-div").html(upcoming);
 			},
 			error: function(data){
 				var resp = $(data);
@@ -40,18 +37,33 @@ $(document).ready(function() {
 			url: 'https://lms.neumont.edu/calendar',
 			success: function(data){
 				var response = $(data);
-				var upcoming = response.find(".mini_month");
-				$(".calendar-div").html(upcoming);
+				var cal = response.find(".mini_month");
+				cal.find("img").css('display','none');
+				$(".calendar-div").html(cal);
 			},
 			error: function(data){
 				var resp = $(data);
+				console.log(resp);
 				$(".calendar-div").html("<span style='color: red'>Error retrieving your calendar.</span>");
 			}
 		});
     }
 	
 	function getGrades(){
-		$(".grade-summary-div").load("https://lms.neumont.edu/grades .course_details");
+		$.ajax({
+			type: 'GET',
+			url: 'https://lms.neumont.edu/grades',
+			success: function(data){
+				var response = $(data);
+				var grades = response.find(".course_details");
+				$(".grade-summary-div").html(grades);
+			},
+			error: function(data){
+				var resp = $(data);
+				console.log(resp);
+				$(".grade-summary-div").html("<span style='color: red'>Error retrieving your grades.</span>");
+			}
+		});
 	}
 	
 	removeElements();
